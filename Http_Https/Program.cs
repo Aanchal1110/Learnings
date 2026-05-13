1358a773a7ce2b56f3dc56d6b3a897495a95653c
Aanchal <aanchalupadhyay1009@gmail.com>
2026-05-13 06:46:54 +0530

    Wrote code to understand http status

--- Http_Https/Program.cs
+++ Http_Https/Program.cs
@@ -1,6 +1,24 @@
+//So the HTTP context is a type of object that gets created automatically upon receiving the request.
+//So when the browser sends a request to the Kestrel and the Kestrel forwards the same request to the application code to the ASP .NET Core application, then ASP.NET Core automatically creates an object of type HTTP context and this context contains the information related to your request, response and many more other details.
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.Run(async (HttpContext context) =>
+{
+    if (1 == 1)
+    {
+        context.Response.StatusCode = 400;
+    }
+    else
+    {
+        context.Response.StatusCode = 200;
+    }
+    await context.Response.WriteAsync("Hello");
+    await context.Response.WriteAsync(" World");
+});
+
+
+//app.MapGet("/", () => "Hello World!");
 
 app.Run();
