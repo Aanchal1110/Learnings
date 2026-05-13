53724502f63ac133380665b9233631580a2afb01
Aanchal <aanchalupadhyay1009@gmail.com>
2026-05-13 11:03:00 +0530

    Learned about the QueryString

--- Http_Https/Program.cs
+++ Http_Https/Program.cs
@@ -8,12 +8,28 @@ app.Run(async (HttpContext context) =>
 {
     if (1 == 1)
     {
+        context.Response.StatusCode = 400;
+        string path = context.Request.Path;
+        string method = context.Request.Method;
         //statusCode should be 200 but for learning purpose we are making it 400
         context.Response.Headers["MyKey"] = "my value";
         context.Response.Headers["Server"] = "My Server";
         context.Response.Headers["Content-Type"] = "text/html";
 
-        context.Response.StatusCode = 400;
+        await context.Response.WriteAsync($"<p>{path}</p>");
+        await context.Response.WriteAsync($"<p>{method}</p>");
+
+
+        if (context.Request.Method == "GET")
+        {
+            if (context.Request.Query.ContainsKey("id"))
+            {
+                string id = context.Request.Query["id"];
+                context.Response.WriteAsync($"<p>{id}</p>");
+            }
+        }
+        
+        
     }
     else
     {
