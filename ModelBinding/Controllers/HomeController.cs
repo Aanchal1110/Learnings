474ac760125561b61e15c21623a721929c62bc1a
Aanchal <aanchalupadhyay1009@gmail.com>
2026-05-24 13:26:45 +0530

    learned about what is model binding

---
+++ ModelBinding/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        //http://localhost:5013/bookstore/?bookid=10&isloggedin=true
+
+        [Route("bookstore/{bookid?}/{isloggedin?}")]
+        public IActionResult Index([FromQuery]int? bookid, [FromRoute]bool? isloggedin, Book book)
+        {
+            if (bookid.HasValue == false)
+            {
+                return StatusCode(400, "Book Id is required");
+            }
+            if (bookid <= 0)
+            {
+                return StatusCode(401, "Book Id must be greater than 0");
+            }
+            if (bookid >= 1000)
+            {
+                return StatusCode(402, "Book Id mustn't be greater than 1000");
+            }
+
+            if (isloggedin.HasValue == false)
+            {
+                return StatusCode(403, "IsLoggedIn is required");
+            }
+
+            return Content($"User logged in with Id - {bookid}, {book}");
+        }
+    }
+}
---
+++ ModelBinding/Models/Book.cs
@@ -0,0 +1,20 @@
+﻿
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Models
+{
+    public class Book
+    {
+        //[FromQuery]
+        public int? BookId { get; set; }
+
+        public string? Author { get; set;  }
+
+        public override string ToString()
+        {
+            return $"Book object -> Book Id -{BookId}, Author - {Author}";
+        }
+    }
+
+}
