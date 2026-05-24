a702078588385432bf446d309449316b8dca7886
Aanchal <aanchalupadhyay1009@gmail.com>
2026-05-24 12:25:28 +0530

    Learned how to provide custom Validation error and how to print or return them

--- ModelBinding/ModelBindingExample/Controllers/HomeController.cs
+++ ModelBinding/ModelBindingExample/Controllers/HomeController.cs
@@ -9,6 +9,31 @@ namespace ModelBindingExample.Controllers
         public IActionResult Index(Person person)
         {
 
+            if (!ModelState.IsValid)
+            {
+                //List<string> errors = new List<string>();
+                //    foreach(var value in ModelState.Values)
+                //    {
+                //        foreach(var error in value.Errors)
+                //        {
+                //            errors.Add(error.ErrorMessage);
+                //        }
+                //    }
+                //    string errorsstring=string.Join("\n", errors
+                //        );
+                //    return Content(errorsstring);
+                //}
+
+                //above can also be written as below
+
+                List<string> errorList = ModelState.Values.SelectMany(value => value.Errors).Select(err => err.ErrorMessage).ToList();
+
+                string errorstring = string.Join("\n", errorList);
+
+                return Content(errorstring);
+
+                    }
+            
             return Content($"{person}");
         }
     }
--- ModelBinding/ModelBindingExample/Models/Person.cs
+++ ModelBinding/ModelBindingExample/Models/Person.cs
@@ -1,9 +1,13 @@
-﻿namespace ModelBindingExample.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ModelBindingExample.Models
 {
     public class Person
     {
+        [Required(ErrorMessage ="Person name should be provided friend")]
         public string? PersonName { get; set; }
 
+        [Required]
         public string? Email { get; set; }
 
         public string? Phone { get; set; }
