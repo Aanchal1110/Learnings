a85aee06445734f4f070f631f9e56fab4d37c729
Aanchal <aanchalupadhyay1009@gmail.com>
2026-05-28 13:23:45 +0530

    performed custom validation of entering a proper date of birth

---
+++ ModelBinding/ModelBindingExample/CustomValidator/MinimumYearValidator.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ModelBindingExample.CustomValidator
+{
+    public class MinimumYearValidator:ValidationAttribute
+    {
+        protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+        {
+            DateTime date=(DateTime)value;
+            if (date.Year <= 2000)
+            {
+                return new ValidationResult("Minimum year should be greater than 2000");
+            }
+
+            else
+            {
+                return ValidationResult.Success;
+
+            }
+        }
+    }
+}
--- ModelBinding/ModelBindingExample/Models/Person.cs
+++ ModelBinding/ModelBindingExample/Models/Person.cs
@@ -1,19 +1,30 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using ModelBindingExample.CustomValidator;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace ModelBindingExample.Models
 {
     public class Person
     {
-        [Required(ErrorMessage ="Person name should be provided friend")]
+        [Required(ErrorMessage = "Person name should be provided friend")]
+        [DisplayName("Person Name")]
+        [StringLength(40, MinimumLength = 3, ErrorMessage = "Person name should be between {2} and {1} characters")]
+
+        [RegularExpression("^[A-Za-z .]*$", ErrorMessage = "{0} should contain only alphabets, space and dot(.)")]
         public string? PersonName { get; set; }
 
-        [Required]
+        [EmailAddress(ErrorMessage = "{0} should be a proper email address")]
         public string? Email { get; set; }
 
+        [Phone(ErrorMessage = "Please enter a proper phone number")]
         public string? Phone { get; set; }
 
+        [Required(ErrorMessage = "Please enter the password")]
         public string? Password { get; set; }
 
+        [Required(ErrorMessage = "Please enter the confirm password")]
+        [Compare("Password", ErrorMessage = "{0} and Password don't match")]
+        [DisplayName("Re- enter password")]
         public string? ConfirmPassword { get; set; }
 
         public double? Price { get; set; }
@@ -22,6 +33,9 @@ namespace ModelBindingExample.Models
         {
             return $"Person Name - {PersonName}, Email - {Email}, Phone - {Phone}, Password - {Password}, ConfirmPassword - {ConfirmPassword}, Price - {Price}";
         }
+
+        [MinimumYearValidator]
+        public DateTime? DOB { get; set; }
 
     }
 }
